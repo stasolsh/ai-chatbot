3707daa48310041894d7566fa3f942a90a6dbb49
Stas <stasolsh@gmail.com>
2026-08-05 11:05:51 +0200

    add Global Exception Handler

---
+++ src/main/java/com/example/aichatbot/exception/UnsupportedFileTypeException.java
@@ -0,0 +1,7 @@
+package com.example.aichatbot.exception;
+
+public class UnsupportedFileTypeException extends RuntimeException {
+    public UnsupportedFileTypeException(String message) {
+        super(message);
+    }
+}
